723e0cdcbfecd70b31df4ec771730ba72d5979c2
Derkk117 <derkk117@gmail.com>
2021-04-27 23:39:25 -0500

    inappwebview and search bar

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:Search_Alerts/util/Browser.dart';
 import 'package:Search_Alerts/domain/user.dart';
 import 'package:Search_Alerts/util/shared_preference.dart';
 import 'package:Search_Alerts/providers/user_provider.dart';
@@ -10,31 +11,162 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  String searchQuery = "";
+  String myurl = "";
+  TextEditingController _txController = new TextEditingController();
+
+  List<Browser> dynamicList = [];
+
+  @override
+  void initState() {
+    dynamicList = [];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    //   return Scaffold(
+    //     appBar: AppBar(
+    //       title: Text("DASHBOARD PAGE"),
+    //       elevation: 0.1,
+    //     ),
+    //     body: Column(
+    //       children: [
+    //         SizedBox(
+    //           height: 100,
+    //         ),
+    //         Center(child: Text(user.email ?? '')),
+    //         SizedBox(height: 100),
+    //         RaisedButton(
+    //           onPressed: () {
+    //             UserPreferences().removeUser();
+    //             Navigator.pushReplacementNamed(context, '/login');
+    //           },
+    //           child: Text("Logout"),
+    //           color: Colors.lightBlueAccent,
+    //         )
+    //       ],
+    //     ),
+    //   );
+    // }
+    //
+    Size size = MediaQuery.of(context).size;
     User user = Provider.of<UserProvider>(context).user;
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        title: Text("DASHBOARD PAGE"),
-        elevation: 0.1,
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 100,
+        title: Container(
+          child: TextField(
+            controller: _txController,
+            autofocus: false,
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.transparent,
+              hintText: 'Something to search?',
+              hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+            ),
           ),
-          Center(child: Text(user.email ?? '')),
-          SizedBox(height: 100),
-          RaisedButton(
-            onPressed: () {
-              UserPreferences().removeUser();
-              Navigator.pushReplacementNamed(context, '/login');
-            },
-            child: Text("Logout"),
-            color: Colors.lightBlueAccent,
-          )
+          height: 40,
+        ),
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: addDynamic,
+                child: Icon(
+                  Icons.search,
+                  size: 26.0,
+                ),
+              )),
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(Icons.more_vert),
+              )),
         ],
+      ),
+      body: ListView.builder(
+        itemCount: dynamicList.length,
+        itemBuilder: (_, index) => dynamicList[index],
       ),
     );
   }
+
+  void addDynamic() {
+    setState(() {
+      dynamicList = [];
+      searchQuery = _txController.text;
+    });
+    print(searchQuery);
+    if (searchQuery.length > 0 && dynamicList.length == 0) {
+      searchQuery = _txController.text;
+      dynamicList.add(Browser(
+          firstUrl:
+              "https://www.google.com.mx/search?sxsrf=ALeKk03zTLWKxs8rq70CbOENqaoWWsMmSA%3A1584863771703&source=hp&ei=Gxp3XtzsKMSMtAb7xJaIDQ&q=",
+          lasttUrl:
+              "&btnK=Google+Search&oq=cambiar+motor+de+busqueda+edge+&gs_l=psy-ab.3.0.0i203l2j0i22i30l8.1351.5047..6282...0.0..0.282.2572.0j14j3......0....1..gws-wiz.......35i39j0i273j0j0i67j0i20i263.I-gqQr5JB1g",
+          searchQuery: this.searchQuery,
+          source:
+              "window.document.getElementsByClassName('WE0UJf')[0].innerText;",
+          domainName: "Google"));
+      dynamicList.add(Browser(
+          firstUrl: "https://www.youtube.com/results?search_query=",
+          lasttUrl: "",
+          searchQuery: this.searchQuery,
+          source:
+              "window.document.getElementsById('text-container')[0].innerText;",
+          domainName: "YouTube"));
+      dynamicList.add(Browser(
+          firstUrl: "https://www.bing.com/search?q=",
+          lasttUrl: "",
+          searchQuery: this.searchQuery,
+          source:
+              "window.document.getElementsByClassName('sb_count')[0].innerText;",
+          domainName: "Bing"));
+    } else if (searchQuery.length > 0 && dynamicList.length > 0) {
+      dynamicList = [];
+    }
+  }
+
+  /*Widget _search() {
+    searchQuery = _txController.text;
+    if (searchQuery.length > 0) {
+      return Container(
+          child: Column(children: <Widget>[
+        Browser(
+            firstUrl:
+                "https://www.google.com.mx/search?sxsrf=ALeKk03zTLWKxs8rq70CbOENqaoWWsMmSA%3A1584863771703&source=hp&ei=Gxp3XtzsKMSMtAb7xJaIDQ&q=",
+            lasttUrl:
+                "&btnK=Google+Search&oq=cambiar+motor+de+busqueda+edge+&gs_l=psy-ab.3.0.0i203l2j0i22i30l8.1351.5047..6282...0.0..0.282.2572.0j14j3......0....1..gws-wiz.......35i39j0i273j0j0i67j0i20i263.I-gqQr5JB1g",
+            searchQuery: this.searchQuery,
+            source:
+                "window.document.getElementsByClassName('WE0UJf')[0].innerText;",
+            domainName: "Google"),
+        SizedBox(
+          height: 25,
+        ),
+        Browser(
+            firstUrl: "https://www.youtube.com/results?search_query=",
+            lasttUrl: "",
+            searchQuery: this.searchQuery,
+            source:
+                "window.document.getElementsById('text-container')[0].innerText;",
+            domainName: "YouTube"),
+        SizedBox(
+          height: 25,
+        ),
+        Browser(
+            firstUrl: "https://www.bing.com/search?q=",
+            lasttUrl: "",
+            searchQuery: this.searchQuery,
+            source:
+                "window.document.getElementsByClassName('sb_count')[0].innerText;",
+            domainName: "Bing"),
+      ]));
+    } else {
+      return Text("What are you looking for?");
+    }
+  }*/
 }
---
+++ lib/util/Browser.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class Browser extends StatefulWidget {
+  final String firstUrl; //first url part.
+  final String lasttUrl; //last url part.
+  final String searchQuery; //key words that we'll search
+  final String domainName; //name of the page.
+  final String source; //element we'll be serching.
+
+  Browser(
+      {Key key,
+      @required this.firstUrl,
+      @required this.lasttUrl,
+      @required this.searchQuery,
+      @required this.source,
+      this.domainName})
+      : super(key: key);
+
+  @override
+  _BrowserState createState() => _BrowserState();
+}
+
+class _BrowserState extends State<Browser> {
+  InAppWebView browser;
+  InAppWebViewController webView;
+  String text = ""; //text that we'll return.
+  Future<String> _calculation;
+
+  @override
+  void initState() {
+    browser = InAppWebView(
+        initialUrl: widget.firstUrl + widget.searchQuery + widget.lasttUrl,
+        initialHeaders: {},
+        initialOptions: InAppWebViewGroupOptions(
+          crossPlatform: InAppWebViewOptions(
+              preferredContentMode: UserPreferredContentMode.DESKTOP,
+              debuggingEnabled: true,
+              userAgent:
+                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/61.0"),
+        ),
+        onWebViewCreated: (InAppWebViewController controller) {
+          webView = controller;
+        },
+        onLoadStart: (InAppWebViewController controller, String url) {},
+        onLoadStop: (InAppWebViewController controller, String url) async {
+          webView = controller;
+          // if JavaScript is enabled, you can use.
+          this.text =
+              await controller.evaluateJavascript(source: widget.source);
+        });
+
+    _calculation = Future<String>.delayed(Duration(seconds: 15), () => text);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: Column(
+              children: [Expanded(child: browser)],
+            ),
+            height: 100,
+          ),
+          FutureBuilder(
+            future: _calculation,
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return new Text('Network connection loss.');
+                case ConnectionState.waiting:
+                  return new Text('Awaiting result...');
+                default:
+                  if (snapshot.hasError)
+                    return new Text('Error: ${snapshot.error}');
+                  else {
+                    if (text != null)
+                      return new Text(widget.domainName + ": " + text ??
+                          "Answer" + ": " + text);
+                    else
+                      return new Text(widget.domainName + ": did not respond" ??
+                          "Unanswer");
+                  }
+              }
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
